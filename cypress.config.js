867de2ac080e43a0ce892b753699e5e75914dd58
mohamedatef2020 <mohamed.atef.pd2020@gmail.com>
2024-06-12 19:52:37 +0300

    update cypress config to record video

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,12 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    video: true,
+    videosFolder: 'cypress/videos',
+    videoUploadOnPasses: true,
+    viewportHeight: 900,
+    viewportWidth: 1200,
+    watchForFileChanges: false,
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
